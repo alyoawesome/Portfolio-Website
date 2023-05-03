b86ee40532e3605435d1956844fa9bd676067cf9
Alex <alyoawesome100@gmail.com>
2023-05-03 03:00:00 -0700

    made changes to deploy on netifly instead

---
+++ dist/index.5ec133c2.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){particleground(document.getElementById("particles"),{dotColor:"#5cbdaa",lineColor:"#5cbdaa"});var e=document.getElementById("intro");e.style.marginTop=-e.offsetHeight/2+"px"}),!1);
+//# sourceMappingURL=index.5ec133c2.js.map
